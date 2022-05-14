e0e513789776cdcea95f59f167d9295148357b08
Jatin Dixit <jatindixit1305@gmail.com>
2022-05-14 23:20:08 +0530

    smmoth scrolling

--- Basics/app8.js
+++ Basics/app8.js
@@ -123,3 +123,15 @@ const isNumber = (val) => typeof val === "number" && val === val;
 isNumber(1); // true
 isNumber("1"); // false
 isNumber(NaN); // false
+
+/*Smoothly scrolls the element on which it's called into the visible area of the browser window.
+
+Use Element.scrollIntoView() to scroll the element.
+Use { behavior: 'smooth' } to scroll smoothly. */
+const smoothScroll = (element) =>
+  document.querySelector(element).scrollIntoView({
+    behavior: "smooth",
+  });
+smoothScroll("#fooBar"); // scrolls smoothly to the element with the id fooBar
+smoothScroll(".fooBar");
+// scrolls smoothly to the first element with a class of fooBar
