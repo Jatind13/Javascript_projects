fb4e02720770223c0f57dce29500ff16e6d7dc15
Jatin Dixit <jatindixit1305@gmail.com>
2022-05-14 23:25:05 +0530

    shiffle array

--- Basics/app8.js
+++ Basics/app8.js
@@ -135,3 +135,17 @@ const smoothScroll = (element) =>
 smoothScroll("#fooBar"); // scrolls smoothly to the element with the id fooBar
 smoothScroll(".fooBar");
 // scrolls smoothly to the first element with a class of fooBar
+/*Randomizes the order of the values of an array, returning a new array.
+
+Use the Fisher-Yates algorithm to reorder the elements of the array. */
+const shuffle = ([...arr]) => {
+  let m = arr.length;
+  while (m) {
+    const i = Math.floor(Math.random() * m--);
+    [arr[m], arr[i]] = [arr[i], arr[m]];
+  }
+  return arr;
+};
+
+const foo = [1, 2, 3];
+shuffle(foo); // [2, 3, 1], foo = [1, 2, 3]
