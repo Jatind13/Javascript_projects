33aea2b5ba4f29f4b03a5f7b8a39eb047e7715f9
Jatin Dixit <jatindixit1305@gmail.com>
2022-05-14 23:13:17 +0530

    check if its number

--- Basics/app8.js
+++ Basics/app8.js
@@ -114,3 +114,12 @@ filterUnique([1, 2, 2, 3, 4, 4, 5]); // [2, 4]
 //this is how you reverse the string using spread operator
 const reverseString = (str) => [...str].reverse().join("");
 reverseString("foobar"); // 'raboof'
+
+//Checks if the given argument is a number.
+
+// Use typeof to check if a value is classified as a number primitive.
+// To safeguard against NaN, check if val === val (as NaN has a typeof equal to number and is the only value not equal to itself).
+const isNumber = (val) => typeof val === "number" && val === val;
+isNumber(1); // true
+isNumber("1"); // false
+isNumber(NaN); // false
