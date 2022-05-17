e6c7b02d9b4a4c6ce769f406b7920a3a79a377b0
Jatin Dixit <jatindixit1305@gmail.com>
2022-05-17 23:25:57 +0530

    covert degree to radian

--- Basics/app8.js
+++ Basics/app8.js
@@ -157,3 +157,11 @@ const detectLanguage = (defaultLang = "en-US") =>
   defaultLang;
 
 console.log(detectLanguage()); // 'nl-NL')
+
+/*Converts an angle from degrees to radians.
+
+Use Math.PI and the degree to radian formula to convert the angle from degrees to radians.*/
+
+const degreesToRads = (deg) => (deg * Math.PI) / 180.0;
+
+degreesToRads(90.0); // ~1.5708
